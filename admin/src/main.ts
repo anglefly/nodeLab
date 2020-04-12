2acc602e826f3820b08fade9950b865c84f84ef0
anglefly <1406909912@qq.com>
2020-04-12 14:39:13 +0800

    改为正常引入avue

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -3,11 +3,11 @@ import axios from 'axios'
 import EleForm from 'vue-ele-form'
 import App from './App.vue'
 import './plugins/element'
+import './plugins/avue'
 import router from './router'
 
 // 注册 vue-ele-form
 Vue.use(EleForm)
-Vue.use((window as any).AVUE);
 
 Vue.config.productionTip = false
 
